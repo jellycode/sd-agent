d7c7cccf162e4476a1077f88ca391d5428c3ca38
shurl <shurl@users.noreply.github.com>
2017-06-07 14:51:04 +0100

    docker tweaks

--- .travis/build_packages.sh
+++ .travis/build_packages.sh
@@ -3,12 +3,13 @@ set -ev
 
 pwd
 ls -alh
-ls -alh
 cd .travis/dockerfiles
 for D in *; do
     if [ -d "${D}" ]; then
         echo "${D}"
-        docker build "${D}"/Dockerfile -t serverdensity:"${D}"
+        cd "${D}"
+        pwd
+        docker build -t serverdensity:"${D}" .
         docker run serverdensity:"${D}"
     fi
 done
