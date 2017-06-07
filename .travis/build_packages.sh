b8143d8bea5bdf6d7a60b27794399b9cb3669896
shurl <shurl@users.noreply.github.com>
2017-06-07 12:31:48 +0100

    add docker service, debug statements

--- .travis/build_packages.sh
+++ .travis/build_packages.sh
@@ -2,7 +2,10 @@
 set -ev
 
 git clone https://github.com/serverdensity/sd-agent.git
-cd sd-agent/.travis/dockerfiles
+ls -alh
+cd sd-agent/
+ls -alh
+cd .travis/dockerfiles
 for D in *; do
     if [ -d "${D}" ]; then
         docker build "${D}"/Dockerfile -t serverdensity:${D}  # your processing here
