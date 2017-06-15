6a5b73bd1540c94319076bf05eab97791cd84e7c
shurl <shurl@users.noreply.github.com>
2017-06-15 17:44:39 +0100

    sudo

--- .travis/build_packages.sh
+++ .travis/build_packages.sh
@@ -3,7 +3,7 @@ set -ev
 
 cd .travis/dockerfiles
 
-mkdir /packages
+sudo mkdir /packages
 
 for d in * ; do (echo "$d" && cd "$d" && docker build -t serverdensity:"${d}" . && docker run serverdensity:"${d}" -v /packages:/packages -v $TRAVIS_BUILD_DIR:/sd-agent); done
 
