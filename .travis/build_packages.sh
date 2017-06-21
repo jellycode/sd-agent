1be9c88229c85c0e6e27029b6b81fa1b396d8e40
shurl <shurl@users.noreply.github.com>
2017-06-21 17:30:58 +0100

    hardcode BUILD_DIR

--- .travis/build_packages.sh
+++ .travis/build_packages.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -ev
 
-cd $TRAVIS_BUILD_DIR
-
-pwd
-ls -alh
-
 cd .travis/dockerfiles
 
 sudo mkdir /packages
 
-for d in * ; do (echo "$d" && cd "$d" && docker build -t serverdensity:"${d}" . && docker run serverdensity:"${d}" -v /packages:/packages -v "$TRAVIS_BUILD_DIR":/sd-agent); done
+for d in * ; do (echo "$d" && cd "$d" && docker build -t serverdensity:"${d}" . && docker run serverdensity:"${d}" -v /packages:/packages -v /home/travis/build/serverdensity/sd-agent:/sd-agent); done
 
 find /packages
 
