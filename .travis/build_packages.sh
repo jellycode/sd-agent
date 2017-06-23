d5df9f0dbd14b925b1d6f63777c2b16c753e8bf9
shurl <shurl@users.noreply.github.com>
2017-06-23 15:19:31 +0100

    add packages volume

--- .travis/build_packages.sh
+++ .travis/build_packages.sh
@@ -8,11 +8,10 @@ sudo mkdir /packages
 for d in * ; do (echo "$d" && cd "$d" && docker build -t serverdensity:"${d}" . && cd ..); done
 
 cd $TRAVIS_BUILD_DIR
-OS=~/build/$TRAVIS_REPO_SLUG
-chmod -R a+w $OS
+
 dockerfile_dir=".travis/dockerfiles/"
 
-for d in .travis/dockerfiles/* ; do (echo "${d#$dockerfile_dir}" && sudo docker run --volume=/home/travis/build/serverdensity/sd-agent:/sd-agent:rw --privileged serverdensity:"${d#$dockerfile_dir}" ); done
+for d in .travis/dockerfiles/* ; do (echo "${d#$dockerfile_dir}" && sudo docker run --volume=/home/travis/build/serverdensity/sd-agent:/sd-agent:rw --volume=/packages:/packages:rw --privileged serverdensity:"${d#$dockerfile_dir}" ); done
 
 find /packages
 
