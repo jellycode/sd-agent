f5781b218ee90f5ab26c817289c77f9b30ae29b2
shurl <shurl@users.noreply.github.com>
2017-06-23 15:55:17 +0100

    check if dir exists

--- .travis/build_packages.sh
+++ .travis/build_packages.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+
+DOCKERFILE_DIR=".travis/dockerfiles/"
+PACKAGES_DIR="/packages"
 set -ev
 
 cd .travis/dockerfiles
 
-sudo mkdir /packages
+if [ -d "$PACKAGES_DIR" ]; then
+    mkdir "$PACKAGES_DIR"
+fi
 
 for d in * ; do (echo "$d" && cd "$d" && docker build -t serverdensity:"${d}" . && cd ..); done
 
 cd $TRAVIS_BUILD_DIR
 
-dockerfile_dir=".travis/dockerfiles/"
-
-for d in .travis/dockerfiles/* ; do (echo "${d#$dockerfile_dir}" && sudo docker run --volume=/home/travis/build/serverdensity/sd-agent:/sd-agent:rw --volume=/packages:/packages:rw --privileged serverdensity:"${d#$dockerfile_dir}" ); done
+for d in .travis/dockerfiles/* ; do (echo "${d#$DOCKERFILE_DIR}" && sudo docker run --volume=/home/travis/build/serverdensity/sd-agent:/sd-agent:rw --volume=/packages:/packages:rw serverdensity:"${d#$DOCKERFILE_DIR}" ); done
 
 find /packages
 
