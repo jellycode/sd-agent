da7423ef9969dad8484255f21f9bc483ebd24bb1
shurl <shurl@users.noreply.github.com>
2017-06-23 12:18:29 +0100

    a+w

--- .travis/build_packages.sh
+++ .travis/build_packages.sh
@@ -8,10 +8,11 @@ sudo mkdir /packages
 for d in * ; do (echo "$d" && cd "$d" && docker build -t serverdensity:"${d}" . && cd ..); done
 
 cd $TRAVIS_BUILD_DIR
-
+OS=~/build/$TRAVIS_REPO_SLUG
+chmod -R a+w $OS
 dockerfile_dir=".travis/dockerfiles/"
 
-for d in .travis/dockerfiles/* ; do (echo "${d#$dockerfile_dir}" &&  docker run serverdensity:"${d#$dockerfile_dir}" -v /packages:/packages -v $PWD:/sd-agent:rw); done
+for d in .travis/dockerfiles/* ; do (echo "${d#$dockerfile_dir}" &&  docker run serverdensity:"${d#$dockerfile_dir}" -v /packages:/packages -v ${OS}:/sd-agent:rw); done
 
 find /packages
 
