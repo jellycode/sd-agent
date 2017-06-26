0a54827a37816586ac85e55dbf70dc5cf8e11fdc
shurl <shurl@users.noreply.github.com>
2017-06-26 17:12:49 +0100

    fix fold command.

--- .travis/build_packages.sh
+++ .travis/build_packages.sh
@@ -16,7 +16,7 @@ docker images -a
 
 cd $TRAVIS_BUILD_DIR
 
-for d in .travis/dockerfiles/* ; do (echo -en "travis_fold:start:run_${d#$DOCKERFILE_DIR}_container\\r" && sudo docker run --volume=/home/travis/build/serverdensity/sd-agent:/sd-agent:rw --volume=/packages:/packages:rw serverdensity:"${d#$DOCKERFILE_DIR}" && ls /packages && echo -en "travis_fold:start:run_${d#$DOCKERFILE_DIR}_container\\r"); done
+for d in .travis/dockerfiles/* ; do (echo -en "travis_fold:start:run_${d#$DOCKERFILE_DIR}_container\\r" && sudo docker run --volume=/home/travis/build/serverdensity/sd-agent:/sd-agent:rw --volume=/packages:/packages:rw serverdensity:"${d#$DOCKERFILE_DIR}" && ls /packages && echo -en "travis_fold:end:run_${d#$DOCKERFILE_DIR}_container\\r"); done
 
 find /packages
 
