4ba4a8d0d723af171c2a42e65674f396b31f1b6f
shurl <shurl@users.noreply.github.com>
2017-06-27 16:07:23 +0100

    first attempt at .deb packaging

--- .travis/build_packages.sh
+++ .travis/build_packages.sh
@@ -9,7 +9,7 @@ cd .travis/dockerfiles
 if [ ! -d "$PACKAGES_DIR" ]; then
     sudo mkdir "$PACKAGES_DIR"
 fi
-
+echo -en "travis_fold:start:rpm_packaging\\r"
 for d in * ; do (echo -en "travis_fold:start:build_${d}_container\\r" && cd "$d" && docker build -t serverdensity:"${d}" . && cd .. && echo -en "travis_fold:end:build_${d}_container\\r"); done
 
 docker images -a
@@ -19,5 +19,16 @@ cd $TRAVIS_BUILD_DIR
 for d in .travis/dockerfiles/* ; do (echo -en "travis_fold:start:run_${d#$DOCKERFILE_DIR}_container\\r" && sudo docker run --volume=/home/travis/build/serverdensity/sd-agent:/sd-agent:rw --volume=/packages:/packages:rw serverdensity:"${d#$DOCKERFILE_DIR}" && ls /packages && echo -en "travis_fold:end:run_${d#$DOCKERFILE_DIR}_container\\r"); done
 
 find /packages
+echo -en "travis_fold:end:rpm_packaging\\r"
+echo -en "travis_fold:start:deb_packaging\\r"
+apt-get update && apt-get install -y pbuilder debootstrap devscripts ubuntu-dev-tools qemu qemu-user-static && mkdir /root/agent-pkg-debian
+echo 'for arch in amd64 i386 armel armhf; do pbuilder-dist precise $arch create; done' > /root/pbuilder-bootstrap.sh
+sh /root/pbuilder-bootstrap.sh
+dpkg-source -b /build/src/sd-agent
+for arch in amd64 i386 armel armhf; do
+    pbuilder-dist precise $arch build \
+    --buildresult "$PACKAGES_DIR"/ubuntu/pool/main/s/sd-agent/ *.dsc
+done
 
-
+find /packages
+echo -en "travis_fold:end:deb_packaging\\r"
