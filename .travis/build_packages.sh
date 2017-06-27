e2d3e62f88ae2f3b20073a3fd586ba50cd8cfa92
shurl <shurl@users.noreply.github.com>
2017-06-27 17:28:22 +0100

    fix dpkg-source

--- .travis/build_packages.sh
+++ .travis/build_packages.sh
@@ -16,7 +16,7 @@ for arch in amd64 i386 armel armhf;
 do
     pbuilder-dist precise $arch create;
 done
-dpkg-source -b /build/src/sd-agent
+dpkg-source -b "$TRAVIS_BUILD_DIR"
 for arch in amd64 i386 armel armhf; do
     pbuilder-dist precise $arch build \
     --buildresult "$PACKAGES_DIR"/ubuntu/pool/main/s/sd-agent/ *.dsc
@@ -29,7 +29,7 @@ echo -en "travis_fold:end:deb_packaging\\r"
 echo -en "travis_fold:start:rpm_packaging\\r"
 for d in * ; do (echo -en "travis_fold:start:build_${d}_container\\r" && cd "$d" && docker build -t serverdensity:"${d}" . && cd .. && echo -en "travis_fold:end:build_${d}_container\\r"); done
 
-cd $TRAVIS_BUILD_DIR
+cd "$TRAVIS_BUILD_DIR"
 
 for d in .travis/dockerfiles/* ; do (echo -en "travis_fold:start:run_${d#$DOCKERFILE_DIR}_container\\r" && sudo docker run --volume=/home/travis/build/serverdensity/sd-agent:/sd-agent:rw --volume=/packages:/packages:rw serverdensity:"${d#$DOCKERFILE_DIR}" && ls /packages && echo -en "travis_fold:end:run_${d#$DOCKERFILE_DIR}_container\\r"); done
 
