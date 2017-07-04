d2517ebb7b6a66bd9a9a6c6142ffdad45ce729d1
shurl <shurl@users.noreply.github.com>
2017-07-04 11:35:16 +0100

    moar debug

--- .travis/upload_packages.sh
+++ .travis/upload_packages.sh
@@ -7,4 +7,5 @@ if [ -f "$REPOSITORY_DIR"  ]; then
     echo 'Yes, we have cache'
     #curl -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/serverdensity/travis-softlayer-object-storage/contents/bootstrap.sh | sed 's|export SLOS_INPUT=${TRAVIS_BUILD_DIR}/build|export SLOS_INPUT=${REPOSITORY_DIR}|g' | /bin/sh
 fi
+find "$REPOSITORY_DIR"
 sudo rm -rf "$REPOSITORY_DIR"
