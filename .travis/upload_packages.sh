d3ce7df08b53de6178f64ba69e540ad048decbb2
shurl <shurl@users.noreply.github.com>
2017-07-04 11:25:14 +0100

    upload packages logic

---
+++ .travis/upload_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+REPOSITORY_DIR="/archive"
+set -ev
+if [ -f "$REPOSITORY_DIR"  ]; then
+    echo 'Yes, we have cache'
+    #curl -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/serverdensity/travis-softlayer-object-storage/contents/bootstrap.sh | sed 's|export SLOS_INPUT=${TRAVIS_BUILD_DIR}/build|export SLOS_INPUT=${REPOSITORY_DIR}|g' | /bin/sh
+fi
+sudo rm -rf "$REPOSITORY_DIR"
