e6ad3268e335046a1d01c7c8dddb7050133c45d6
shurl <shurl@users.noreply.github.com>
2017-07-07 10:41:46 +0100

    Revert "update build_packages.sh for OS X"
    This reverts commit a3231ba2595db0f4cc61a4a22f95f5c6d9202811.

--- .travis/build_packages.sh
+++ .travis/build_packages.sh
@@ -4,106 +4,97 @@ DOCKERFILE_DIR=".travis/dockerfiles/"
 PACKAGES_DIR="/packages"
 REPOSITORY_DIR="/archive"
 set -ev
+cd "$DOCKERFILE_DIR"
 
-if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
-
-    # Only run on default flavor.
-    ./packaging/darwin/build_installer.sh
-
-else
-
-    cd "$DOCKERFILE_DIR"
-
-    #Create required folders if they do not already exist
-    if [ ! -d "$PACKAGES_DIR" ]; then
-        sudo mkdir "$PACKAGES_DIR"
-    fi
-    if [ ! -d "$CACHE_DIR" ]; then
-        sudo mkdir "$CACHE_DIR"
-    fi
-
-    #If the containers are in cache, load them, else create containers (They should be in cache already)
-    for d in * ;
-    do
-        echo -en "travis_fold:start:build_${d}_container\\r"
-        TEMP="\${CACHE_FILE_${d}}"
-        DOCKER_CACHE=$(eval echo "$TEMP")
-        if [ -f "$DOCKER_CACHE"  ]; then
-            gunzip -c "$DOCKER_CACHE" | docker load;
-        else
-            (cd "$d" && docker build -t serverdensity:"${d}" .)
-            if [ ! -f "$DOCKER_CACHE"  ]; then
-                docker save serverdensity:"${d}" | gzip > "$DOCKER_CACHE";
-            fi
-        fi
-
-        echo -en "travis_fold:end:build_${d}_container\\r"
-    done
-
-    #Run the containers, if container name is precise run with --privileged
-
-    for d in * ;
-    do
-        echo "$d"
-        if [[ "$d" == "precise" ]]; then
-            echo -en "travis_fold:start:run_${d}_container\\r"
-            sudo docker run --volume="${TRAVIS_BUILD_DIR}":/sd-agent:rw --volume=/packages:/packages:rw --privileged serverdensity:"${d}"
-            echo -en "travis_fold:end:run_${d}_container\\r"
-        else
-            echo -en "travis_fold:start:run_${d}_container\\r"
-            sudo docker run --volume="${TRAVIS_BUILD_DIR}":/sd-agent:rw --volume=/packages:/packages:rw serverdensity:"${d}"
-            echo -en "travis_fold:end:run_${d}_container\\r"
-        fi
-    done
-
-    # Prepare folder to be come the repository
-    if [ ! -d "$REPOSITORY_DIR" ]; then
-        sudo mkdir "$REPOSITORY_DIR"
-    fi
-
-    if [ ! -d "$REPOSITORY_DIR"/el ]; then
-        sudo mkdir "$REPOSITORY_DIR"/el
-    fi
-
-    if [ ! -d "$REPOSITORY_DIR"/el/5 ]; then
-        sudo mkdir "$REPOSITORY_DIR"/el/5
-        sudo mkdir "$REPOSITORY_DIR"/el/5/x86_64
-        sudo mkdir "$REPOSITORY_DIR"/el/5/i386
-        sudo mkdir "$REPOSITORY_DIR"/el/5/repodata
-    fi
-
-    if [ ! -d "$REPOSITORY_DIR"/ubuntu ]; then
-        sudo mkdir "$REPOSITORY_DIR"/ubuntu
-    fi
-
-    sudo cp -a "$TRAVIS_BUILD_DIR"/packaging/ubuntu/conf/. "$REPOSITORY_DIR"/ubuntu/conf
-
-    # Prepare el packages as repo
-    find "$PACKAGES_DIR"
-    sudo cp -a "$PACKAGES_DIR"/el/. "$REPOSITORY_DIR"/el
-    cd "$REPOSITORY_DIR"/el
-    find "$REPOSITORY_DIR"
-    sudo createrepo 6
-    #sudo createrepo 7
-    #cat << EOF > ~/.rpmmacros
-    #%_topdir /tmp/el
-    #%_tmppath %{_topdir}/tmp
-    #%_signature gpg
-    #%_gpg_name hello@serverdensity.com
-    #%_gpg_path ~/.gnupg
-    #EOF
-    #LC_ALL=C rpm --addsign 6/*/*.rpm 7/*/*.rpm
-
-    # Prepare deb packages as repo
-    cd "$REPOSITORY_DIR"/ubuntu
-    #FOR TESTING
-    sed -i '/SignWith: 131EFC09/d' "$REPOSITORY_DIR"/ubuntu/conf/distributions
-    sed -i '/ask-passphrase/d' "$REPOSITORY_DIR"/ubuntu/conf/options
-
-    sudo reprepro includedeb all "$PACKAGES_DIR"/precise/amd64/sd-agent*.deb "$PACKAGES_DIR"/precise/i386/sd-agent*i386*.deb
-
-    find "$REPOSITORY_DIR"
-
-    curl -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/serverdensity/travis-softlayer-object-storage/contents/bootstrap-generic.sh | sed 's|export SLOS_INPUT=${TRAVIS_BUILD_DIR}|export SLOS_INPUT=${REPOSITORY_DIR}|g' | sed 's:export SLOS_NAME=`echo "${TRAVIS_REPO_SLUG}" | cut -f 2 -d /`:export SLOS_NAME=agent-repo:g' | /bin/sh
-    find /tmp
+#Create required folders if they do not already exist
+if [ ! -d "$PACKAGES_DIR" ]; then
+    sudo mkdir "$PACKAGES_DIR"
 fi
+if [ ! -d "$CACHE_DIR" ]; then
+    sudo mkdir "$CACHE_DIR"
+fi
+
+#If the containers are in cache, load them, else create containers (They should be in cache already)
+for d in * ;
+do
+    echo -en "travis_fold:start:build_${d}_container\\r"
+    TEMP="\${CACHE_FILE_${d}}"
+    DOCKER_CACHE=$(eval echo "$TEMP")
+    if [ -f "$DOCKER_CACHE"  ]; then
+        gunzip -c "$DOCKER_CACHE" | docker load;
+    else
+        (cd "$d" && docker build -t serverdensity:"${d}" .)
+        if [ ! -f "$DOCKER_CACHE"  ]; then
+            docker save serverdensity:"${d}" | gzip > "$DOCKER_CACHE";
+        fi
+    fi
+
+    echo -en "travis_fold:end:build_${d}_container\\r"
+done
+
+#Run the containers, if container name is precise run with --privileged
+
+for d in * ;
+do
+    echo "$d"
+    if [[ "$d" == "precise" ]]; then
+        echo -en "travis_fold:start:run_${d}_container\\r"
+        sudo docker run --volume="${TRAVIS_BUILD_DIR}":/sd-agent:rw --volume=/packages:/packages:rw --privileged serverdensity:"${d}"
+        echo -en "travis_fold:end:run_${d}_container\\r"
+    else
+        echo -en "travis_fold:start:run_${d}_container\\r"
+        sudo docker run --volume="${TRAVIS_BUILD_DIR}":/sd-agent:rw --volume=/packages:/packages:rw serverdensity:"${d}"
+        echo -en "travis_fold:end:run_${d}_container\\r"
+    fi
+done
+
+# Prepare folder to be come the repository
+if [ ! -d "$REPOSITORY_DIR" ]; then
+    sudo mkdir "$REPOSITORY_DIR"
+fi
+
+if [ ! -d "$REPOSITORY_DIR"/el ]; then
+    sudo mkdir "$REPOSITORY_DIR"/el
+fi
+
+if [ ! -d "$REPOSITORY_DIR"/el/5 ]; then
+    sudo mkdir "$REPOSITORY_DIR"/el/5
+    sudo mkdir "$REPOSITORY_DIR"/el/5/x86_64
+    sudo mkdir "$REPOSITORY_DIR"/el/5/i386
+    sudo mkdir "$REPOSITORY_DIR"/el/5/repodata
+fi
+
+if [ ! -d "$REPOSITORY_DIR"/ubuntu ]; then
+    sudo mkdir "$REPOSITORY_DIR"/ubuntu
+fi
+
+sudo cp -a "$TRAVIS_BUILD_DIR"/packaging/ubuntu/conf/. "$REPOSITORY_DIR"/ubuntu/conf
+
+# Prepare el packages as repo
+find "$PACKAGES_DIR"
+sudo cp -a "$PACKAGES_DIR"/el/. "$REPOSITORY_DIR"/el
+cd "$REPOSITORY_DIR"/el
+find "$REPOSITORY_DIR"
+sudo createrepo 6
+#sudo createrepo 7
+#cat << EOF > ~/.rpmmacros
+#%_topdir /tmp/el
+#%_tmppath %{_topdir}/tmp
+#%_signature gpg
+#%_gpg_name hello@serverdensity.com
+#%_gpg_path ~/.gnupg
+#EOF
+#LC_ALL=C rpm --addsign 6/*/*.rpm 7/*/*.rpm
+
+# Prepare deb packages as repo
+cd "$REPOSITORY_DIR"/ubuntu
+#FOR TESTING
+sed -i '/SignWith: 131EFC09/d' "$REPOSITORY_DIR"/ubuntu/conf/distributions
+sed -i '/ask-passphrase/d' "$REPOSITORY_DIR"/ubuntu/conf/options
+
+sudo reprepro includedeb all "$PACKAGES_DIR"/precise/amd64/sd-agent*.deb "$PACKAGES_DIR"/precise/i386/sd-agent*i386*.deb
+
+find "$REPOSITORY_DIR"
+
+curl -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/serverdensity/travis-softlayer-object-storage/contents/bootstrap-generic.sh | sed 's|export SLOS_INPUT=${TRAVIS_BUILD_DIR}|export SLOS_INPUT=${REPOSITORY_DIR}|g' | sed 's:export SLOS_NAME=`echo "${TRAVIS_REPO_SLUG}" | cut -f 2 -d /`:export SLOS_NAME=agent-repo:g' | /bin/sh
+find /tmp
