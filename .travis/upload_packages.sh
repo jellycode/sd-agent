22a3893b46f89cac2aa8e7258b7a0c453d51d6c1
shurl <shurl@users.noreply.github.com>
2017-07-10 19:08:33 +0100

    move debug line

--- .travis/upload_packages.sh
+++ .travis/upload_packages.sh
@@ -24,7 +24,7 @@ if [ -f "$CACHE_FILE_PACAKGES_MAC"  ]; then
     sudo tar -zxvf "$CACHE_FILE_PACAKGES_MAC" -C "$REPOSITORY_DIR"
 fi
 
-find "$REPOSITORY_DIR"
+
 
 #Get el5 repo as we are no longer building packages for it.
 for arch in ${EL5_ARCHTYPES[*]};
@@ -41,3 +41,4 @@ do
 done
 
 #curl -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/serverdensity/travis-softlayer-object-storage/contents/bootstrap-generic.sh | sed 's|export SLOS_INPUT=${TRAVIS_BUILD_DIR}|export SLOS_INPUT=${REPOSITORY_DIR}|g' | sed 's:export SLOS_NAME=`echo "${TRAVIS_REPO_SLUG}" | cut -f 2 -d /`:export SLOS_NAME=agent-repo:g' | /bin/sh
+find "$REPOSITORY_DIR"
